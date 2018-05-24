8db82e427313f560f94886803af7a9774dfdc03e
Marek Linka <marek.linka@outlook.com>
2018-05-24 10:10:27 +0200

    Further network size reduction

--- src/train_model.py
+++ src/train_model.py
@@ -19,7 +19,7 @@ def train(trainig_data_path):
     """
 
     model = Sequential()
-    model.add(Conv2D(32, (5, 5), strides=(2,2), input_shape=(c.image_height, c.image_width, 3)))
+    model.add(Conv2D(32, (5, 5), strides=(2,2), input_shape=(c.image_height, c.image_width, 3),kernel_regularizer=regularizers.l2(0.001)))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -40,7 +40,7 @@ def train(trainig_data_path):
 
     model.add(Flatten())
 
-    model.add(Dense(192))
+    model.add(Dense(64))
     
     # model.add(BatchNormalization())
     model.add(Activation('relu'))
@@ -50,8 +50,8 @@ def train(trainig_data_path):
 
     model.summary()
 
-    # tbCallback = TensorBoard(log_dir='./TB', histogram_freq=0, write_graph=True, write_images=True)
+    tbCallback = TensorBoard(log_dir='./TB', histogram_freq=0, write_graph=True, write_images=True)
     cpCallback = ModelCheckpoint("models\\current_model_best.h5", save_best_only=True, monitor="val_loss", mode="min", save_weights_only=False)
-    model.fit_generator(gen.training_data(trainig_data_path), epochs=100, steps_per_epoch=160, validation_data=gen.training_data("data\\validation"),validation_steps=12, callbacks=[cpCallback])
+    model.fit_generator(gen.training_data(trainig_data_path), epochs=100, steps_per_epoch=240, validation_data=gen.training_data("data\\validation"),validation_steps=30, callbacks=[cpCallback, tbCallback])
 
     model_storage.save_model(model, "current_model")
